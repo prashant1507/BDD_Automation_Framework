9025d6f22ca8aa4e043ce67b712ccb0389dcb76d
prashant1507 <pjeet.0733@gmail.com>
2021-11-29 20:40:50 +0400

    Minor Changes

--- src/main/java/org/automation/pages/HomePage.java
+++ src/main/java/org/automation/pages/HomePage.java
@@ -3,10 +3,12 @@ package org.automation.pages;
 import org.automation.factories.BrowserOperationFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public final class HomePage extends BasePage {
-	
-	private final By welcomeUserText = By.xpath("//div[@id='branding']/a[@id='welcome']");
+
+	@FindBy(xpath = "//div[@id='branding']/a[@id='welcome']")
+	private By welcomeUserText;
 	
 	public WebElement getWelcomeUser() {
 		return getWebElement(welcomeUserText);
--- src/main/java/org/automation/pages/LoginPage.java
+++ src/main/java/org/automation/pages/LoginPage.java
@@ -1,15 +1,27 @@
 package org.automation.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 
 public final class LoginPage extends BasePage {
 
-	private final By pageLogoImg = By.xpath("//div[@id='divLogo']/img");
-	private final By usernameTextBox = By.id("txtUsername");
-	private final By passwordTextBox = By.id("txtPassword");
-	private final By loginBtn = By.xpath("//input[@id='btnLogin']");
-	private final By forgotPasswordLink = By.xpath("//div[@id='forgotPasswordLink']/a");
-	private final By invalidCredentialsErrorMsg = By.xpath("//span[@id='spanMessage']");
+	@FindBy(xpath = "//div[@id='divLogo']/img")
+	private By pageLogoImg;
+
+	@FindBy(id = "txtUsername")
+	private By usernameTextBox;
+
+	@FindBy(id = "txtPassword")
+	private By passwordTextBox;
+
+	@FindBy(xpath = "//input[@id='btnLogin']")
+	private By loginBtn;
+
+	@FindBy(xpath = "//div[@id='forgotPasswordLink']/a")
+	private By forgotPasswordLink;
+
+	@FindBy(xpath = "//span[@id='spanMessage']")
+	private By invalidCredentialsErrorMsg;
 
 	public boolean isPageLogoImgDisplayed() {
 		return isDisplayed(pageLogoImg);
